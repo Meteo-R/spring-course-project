fc5c73e8e611a44ee14f42f390b42748a4a4e945
Meteo-R <reda.mateusz@gmail.com>
2020-08-19 11:17:46 +0200

    S5L38 Dependency Injection XML config - injecting literal values

--- src/main/java/com/mr/first/SetterDemoApp.java
+++ src/main/java/com/mr/first/SetterDemoApp.java
@@ -6,10 +6,12 @@ public class SetterDemoApp {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Coach coach = context.getBean("myCricketCoach", CricketCoach.class);
+        CricketCoach coach = context.getBean("myCricketCoach", CricketCoach.class);
 
         System.out.println(coach.getDailyWorkout());
         System.out.println(coach.getDailyFortune());
+        System.out.println(coach.getEmailAddress());
+        System.out.println(coach.getTeam());
 
         context.close();
     }
