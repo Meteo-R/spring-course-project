37989263106b3fc3a4f6ba886beca73043876318
Meteo-R <reda.mateusz@gmail.com>
2020-08-19 10:29:32 +0200

    S5L36 Dependency Injection XML configuration - setter injection

---
+++ src/main/java/com/mr/first/SetterDemoApp.java
@@ -0,0 +1,16 @@
+package com.mr.first;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        Coach coach = context.getBean("myCricketCoach", CricketCoach.class);
+
+        System.out.println(coach.getDailyWorkout());
+        System.out.println(coach.getDailyFortune());
+
+        context.close();
+    }
+}
