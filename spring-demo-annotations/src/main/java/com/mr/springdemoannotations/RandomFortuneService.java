223962b3e0d8a305f211b7e847944b2125382a19
Meteo-R <reda.mateusz@gmail.com>
2020-08-20 13:09:55 +0200

    S8L71 Implementation of RandomFortuneService

--- spring-demo-annotations/src/main/java/com/mr/springdemoannotations/RandomFortuneService.java
+++ spring-demo-annotations/src/main/java/com/mr/springdemoannotations/RandomFortuneService.java
@@ -2,11 +2,22 @@ package com.mr.springdemoannotations;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 @Component
 public class RandomFortuneService implements FortuneService {
 
+    private static final List<String> FORTUNES = Arrays.asList(
+            "Your limitation - it's only your imagination",
+            "Push yourself, because no one else is going to do it for you",
+            "Sometimes later becomes never"
+    );
+
     @Override
     public String getFortune() {
-        return null;
+        int random = ThreadLocalRandom.current().nextInt(0, FORTUNES.size());
+        return FORTUNES.get(random);
     }
 }
