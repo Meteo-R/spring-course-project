f8482b28c283e110e8faf163e51ce5c51febb9ff
Meteo-R <reda.mateusz@gmail.com>
2020-08-20 13:15:29 +0200

    S8L70 DI with Annotations - use of @Qualifier with constructor injection

--- spring-demo-annotations/src/main/java/com/mr/springdemoannotations/TennisCoach.java
+++ spring-demo-annotations/src/main/java/com/mr/springdemoannotations/TennisCoach.java
@@ -7,18 +7,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
-    @Autowired
-    @Qualifier("randomFortuneService")
+//    @Autowired
     private FortuneService fortuneService;
 
-    public TennisCoach() {
-        System.out.println(">> TennisCoach: inside default constructor");
-    }
-
-//    @Autowired
-//    public TennisCoach(FortuneService fortuneService) {
-//        this.fortuneService = fortuneService;
+//    public TennisCoach() {
+//        System.out.println(">> TennisCoach: inside default constructor");
 //    }
+
+    @Autowired
+    public TennisCoach(@Qualifier("randomFortuneService") FortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
 
     @Override
     public String getDailyWorkout() {
