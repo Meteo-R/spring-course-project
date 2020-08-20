ec09a12cdf65af091dbc819481a9d6e69172f916
Meteo-R <reda.mateusz@gmail.com>
2020-08-20 12:16:17 +0200

    S8L70 DI with Annotations - choosing implementation with @Qualifier

---
+++ spring-demo-annotations/src/main/java/com/mr/springdemoannotations/RandomFortuneService.java
@@ -0,0 +1,12 @@
+package com.mr.springdemoannotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
---
+++ spring-demo-annotations/src/main/java/com/mr/springdemoannotations/RestFortuneService.java
@@ -0,0 +1,12 @@
+package com.mr.springdemoannotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestFortuneService implements FortuneService {
+
+    @Override
+    public String getFortune() {
+        return null;
+    }
+}
