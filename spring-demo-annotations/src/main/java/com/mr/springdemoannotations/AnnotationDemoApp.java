ad1d6cc98141b56b47ce3bd6708d2e7d516fd919
Meteo-R <reda.mateusz@gmail.com>
2020-08-20 13:30:56 +0200

    S8L74 Injecting values from properties file with @Value annotation

--- spring-demo-annotations/src/main/java/com/mr/springdemoannotations/AnnotationDemoApp.java
+++ spring-demo-annotations/src/main/java/com/mr/springdemoannotations/AnnotationDemoApp.java
@@ -6,11 +6,13 @@ public class AnnotationDemoApp {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Coach coach = context.getBean("tennisCoach", Coach.class);
+        TennisCoach coach = context.getBean("tennisCoach", TennisCoach.class);
 
         System.out.println();
         System.out.println(coach.getDailyWorkout());
         System.out.println(coach.getDailyFortune());
+        System.out.println(coach.getEmail());
+        System.out.println(coach.getTeam());
         System.out.println();
 
         context.close();
--- spring-demo-annotations/src/main/java/com/mr/springdemoannotations/TennisCoach.java
+++ spring-demo-annotations/src/main/java/com/mr/springdemoannotations/TennisCoach.java
@@ -2,6 +2,7 @@ package com.mr.springdemoannotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +10,12 @@ public class TennisCoach implements Coach {
 
 //    @Autowired
     private FortuneService fortuneService;
+
+    @Value("${foo.email}")
+    private String email;
+
+    @Value("${foo.team}")
+    private String team;
 
 //    public TennisCoach() {
 //        System.out.println(">> TennisCoach: inside default constructor");
@@ -27,6 +34,14 @@ public class TennisCoach implements Coach {
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTeam() {
+        return team;
     }
 
 //    @Autowired
