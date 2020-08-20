b41b349d067ecedae91d56f523d04fe67fa49a6b
Meteo-R <reda.mateusz@gmail.com>
2020-08-20 14:34:27 +0200

    S10L89 Spring Configuration with Java - @Bean

--- spring-demo-annotations/src/main/java/com/mr/springdemoannotations/SportConfig.java
+++ spring-demo-annotations/src/main/java/com/mr/springdemoannotations/SportConfig.java
@@ -1,10 +1,21 @@
 package com.mr.springdemoannotations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.mr.springdemoannotations")
+//@ComponentScan("com.mr.springdemoannotations")
 public class SportConfig {
+
+    @Bean
+    public FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
+
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
 
 }
