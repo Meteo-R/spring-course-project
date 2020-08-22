4688488c9e90b2ebf2dbe639380a09ed5eec0e1e
Meteo-R <reda.mateusz@gmail.com>
2020-08-22 22:34:26 +0200

    S57L454 Jackson deserialization from JSON - nested objects, arrays

--- jackson-databind-json-demo/src/main/java/com/mr/jackson/json/demo/Driver.java
+++ jackson-databind-json-demo/src/main/java/com/mr/jackson/json/demo/Driver.java
@@ -10,10 +10,19 @@ public class Driver {
         try {
             ObjectMapper mapper = new ObjectMapper();
 
-            Student student = mapper.readValue(new File("jackson-databind-json-demo/src/main/resources/sample-lite.json"), Student.class);
+            Student student = mapper.readValue(new File("jackson-databind-json-demo/src/main/resources/sample-full.json"), Student.class);
 
             System.out.println("First name = " + student.getFirstName());
             System.out.println("Last name  = " + student.getLastName());
+
+            Address address = student.getAddress();
+
+            System.out.println("Street = " + address.getStreet());
+            System.out.println("Street = " + address.getCity());
+
+            for (String language : student.getLanguages()) {
+                System.out.println(language);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
