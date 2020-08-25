67668bef642015504b319588e14f9d4983b0c0ff
Meteo-R <reda.mateusz@gmail.com>
2020-08-25 21:55:31 +0200

    S58L461 REST controller - configuration

---
+++ spring-rest-demo/src/main/java/com/mr/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,11 @@
+package com.mr.springdemo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.mr.springdemo")
+public class DemoAppConfig {
+}
