e0a9ce70473e10e1e2f448620454ca7d9d310cee
Meteo-R <reda.mateusz@gmail.com>
2020-08-27 21:58:41 +0200

    S61L479 Spring REST - global exception handling with @ControllerAdvice

--- spring-rest-demo/src/main/java/com/mr/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/mr/springdemo/rest/StudentRestController.java
@@ -38,24 +38,4 @@ public class StudentRestController {
         return students.get(studentId);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception) {
-        StudentErrorResponse errorResponse = new StudentErrorResponse();
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setMessage(exception.getMessage());
-        errorResponse.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exception) {
-        StudentErrorResponse errorResponse = new StudentErrorResponse();
-        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setMessage(exception.getMessage());
-        errorResponse.setTimeStamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-    }
-
 }
