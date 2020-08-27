17e6ca0fdf50dc50e04e3bd762e44c4e8f448445
Meteo-R <reda.mateusz@gmail.com>
2020-08-27 21:47:38 +0200

    S61L477 Spring REST - handling more general exception

--- spring-rest-demo/src/main/java/com/mr/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/mr/springdemo/rest/StudentRestController.java
@@ -48,4 +48,14 @@ public class StudentRestController {
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exception) {
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setMessage(exception.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
 }
