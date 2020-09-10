0b49b09bb1374f58c896070405d4fa7a9011e180
Meteo-R <reda.mateusz@gmail.com>
2020-09-10 16:44:38 +0200

    S71L513 Auto reload with Spring Boot Dev Tools

--- 02-dev-tools-demo/src/main/java/com/mr/springboot/demo/devtoolsdemo/rest/FunRestController.java
+++ 02-dev-tools-demo/src/main/java/com/mr/springboot/demo/devtoolsdemo/rest/FunRestController.java
@@ -13,4 +13,14 @@ public class FunRestController {
         return "Hello World! Time on server is " + LocalDateTime.now();
     }
 
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+
 }
