694aa1570f227f4479875905632445b3a7ce95e8
Meteo-R <reda.mateusz@gmail.com>
2020-09-11 14:58:52 +0200

    S73L519 Created Spring Boot module 05-properties-demo

---
+++ 05-properties-demo/src/test/java/com/mr/springboot/demo/properties/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.springboot.demo.properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
