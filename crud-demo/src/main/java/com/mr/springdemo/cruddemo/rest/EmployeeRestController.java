5415baa728cd22d47a4fbe7b86a0fd6e0db999b4
Meteo-R <reda.mateusz@gmail.com>
2020-09-15 22:00:51 +0200

    S74L529 Endpoint for adding new employee

--- crud-demo/src/main/java/com/mr/springdemo/cruddemo/rest/EmployeeRestController.java
+++ crud-demo/src/main/java/com/mr/springdemo/cruddemo/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import com.mr.springdemo.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,13 @@ public class EmployeeRestController {
             throw new RuntimeException("Employee id not found: " + employeeId);
         }
 
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        employee.setId(0);
+        employeeService.save(employee);
         return employee;
     }
 
