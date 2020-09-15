91610b5f018d32a53df23cf64de1ed76531645c8
Meteo-R <reda.mateusz@gmail.com>
2020-09-15 22:08:00 +0200

    S74L530 Endpoint for updating employee

--- crud-demo/src/main/java/com/mr/springdemo/cruddemo/rest/EmployeeRestController.java
+++ crud-demo/src/main/java/com/mr/springdemo/cruddemo/rest/EmployeeRestController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +43,12 @@ public class EmployeeRestController {
     @PostMapping("/employees")
     public Employee addEmployee(@RequestBody Employee employee) {
         employee.setId(0);
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
         employeeService.save(employee);
         return employee;
     }
