508888d54e8d395f2177fcdd7c3b37a45764e1d7
Meteo-R <reda.mateusz@gmail.com>
2020-09-19 21:45:28 +0200

    S77L540 Spring Data REST - customizing endpoint path

--- 24-spring-data-rest-crud-demo/src/main/java/com/mr/springdemo/springdatarestcruddemo/dao/EmployeeRepository.java
+++ 24-spring-data-rest-crud-demo/src/main/java/com/mr/springdemo/springdatarestcruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.mr.springdemo.springdatarestcruddemo.dao;
 
 import com.mr.springdemo.springdatarestcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
